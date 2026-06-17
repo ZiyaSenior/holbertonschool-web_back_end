39dea03e2364e2805711f8047e9aa6b825aa05c1
ZiyaSenior <safaraliyevziya@gmail.com>
2026-06-17 09:18:12 +0000

    tasks

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,22 +6,23 @@ Nginx log statistics from MongoDB
 from pymongo import MongoClient
 
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-collection = client.logs.nginx
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
 
-total_logs = collection.count_documents({})
+    total_logs = collection.count_documents({})
 
-print(f"{total_logs} logs")
-print("Methods:")
+    print(f"{total_logs} logs")
+    print("Methods:")
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-for method in methods:
-    print(f"\tmethod {method}: {collection.count_documents({'method': method})}")
+    for method in methods:
+        print(f"\tmethod {method}: {collection.count_documents({'method': method})}")
 
-status_check = collection.count_documents({
-    "method": "GET",
-    "path": "/status"
-})
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
 
-print(f"{status_check} status check")
+    print(f"{status_check} status check")
