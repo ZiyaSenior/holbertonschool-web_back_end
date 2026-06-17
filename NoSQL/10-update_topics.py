4d620b8776c50e767ad55a0def752c7ada18cd3d
ZiyaSenior <safaraliyevziya@gmail.com>
2026-06-17 09:11:36 +0000

    tasks

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Update topics of a school document
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics of a school document based on name
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
