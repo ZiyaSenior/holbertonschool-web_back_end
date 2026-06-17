81fa7b6ee250b6c0213b9548d04e94a61bc098ba
ZiyaSenior <safaraliyevziya@gmail.com>
2026-06-17 09:12:53 +0000

    tasks

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Find schools by topic
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Return list of schools that contain a specific topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
