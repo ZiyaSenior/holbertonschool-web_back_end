1b95b1e4178cf6422d698b4c771c81fd4f9af9f1
ziyasafaraliyev <safaraliyevziya@gmail.com>
2026-07-20 18:05:57 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
+const stdin = process.openStdin();
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name.trim()}\n`);
+stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
 });
 
-process.stdin.on('end', () => {
+stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.resume();
