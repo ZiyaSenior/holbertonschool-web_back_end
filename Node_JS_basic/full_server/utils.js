cdc4ce6ef5c444b632bdb0fd3c6e38fab48b0a0a
ziyasafaraliyev <safaraliyevziya@gmail.com>
2026-07-20 18:27:32 +0400

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,1 +1,31 @@
+import fs from 'fs';
 
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const record = student.split(',');
+      const firstName = record[0];
+      const field = record[3];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstName);
+    });
+
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
